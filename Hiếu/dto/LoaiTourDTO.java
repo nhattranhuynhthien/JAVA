fef281c6765c66cc43d4934f77f8ab243f31eb0d
Tran Viet Hieu - Hiếu <trhi4100@gmail.com>
2026-03-15 23:28:17 +0700

    Enhance LoaiTourDTO with description and status
    Added description and status fields to LoaiTourDTO.

--- Hiếu/dto/LoaiTourDTO.java
+++ Hiếu/dto/LoaiTourDTO.java
@@ -3,15 +3,21 @@ package org.example.dto;
 public class _LoaiTourDTO {
     private String maLoaiTour;
     private String theLoai;
+    private String moTa;
+    private int trangThai; // 1: Đang hoạt động, 0: Ngưng
 
     public _LoaiTourDTO(){
         this.maLoaiTour = "";
         this.theLoai = "";
+        this.moTa = "";
+        this.trangThai = 1; // mặc định là đang hoạt đông
     }
 
-    public _LoaiTourDTO(String maLoaiTour, String theLoai) {
+    public _LoaiTourDTO(String maLoaiTour, String theLoai, String moTa, int trangThai) {
         this.maLoaiTour = maLoaiTour;
         this.theLoai = theLoai;
+        this.moTa = moTa;
+        this.trangThai = trangThai;
     }
 
     public void setMaLoaiTour(String maLoaiTour) {
@@ -22,6 +28,14 @@ public class _LoaiTourDTO {
         this.theLoai = theLoai;
     }
 
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
     public String getMaLoaiTour() {
         return maLoaiTour;
     }
@@ -29,6 +43,15 @@ public class _LoaiTourDTO {
     public String getTheLoai() {
         return theLoai;
     }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
 
     @Override
     public String toString(){
